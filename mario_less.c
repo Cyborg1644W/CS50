4b5df19a1e9830cf3f56491a86a34612b07d9e16
Reindel Andrada <94912874+Cyborg1644W@users.noreply.github.com>
2026-07-08 23:27:16 +0800

    Add pyramid display program in mario_less.c
    Implement a pyramid display program that prompts the user for height and displays a pyramid structure.

---
+++ mario_less.c
@@ -0,0 +1,48 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// constants
+#define MIN_HEIGHT 1
+#define MAX_HEIGHT 8
+
+// prototype
+int get_height();
+void display_pyramid(int size);
+
+int main(void)
+{
+    int height = get_height();
+    display_pyramid(height);
+
+    return 0;
+}
+
+int get_height()
+{
+    int height = 0;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < MIN_HEIGHT || height > MAX_HEIGHT);
+    return height;
+}
+
+void display_pyramid(int size)
+{
+    int counter = 1; //
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size - counter; j++)
+        { // space
+            printf(" ");
+        }
+        for (int j = 0; j < counter; j++)
+        { // left hashes
+            printf("#");
+        }
+        counter++;
+        printf("\n");
+    }
+}
